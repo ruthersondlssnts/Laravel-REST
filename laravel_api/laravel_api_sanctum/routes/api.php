e5ce2879652733cef500116667bb412f0203f857
ruthersondlssnts <rutherson20@gmail.com>
2021-09-24 14:38:47 +0800

    Models migration, Controller methods

--- laravel_api/laravel_api_sanctum/routes/api.php
+++ laravel_api/laravel_api_sanctum/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UnitController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(['prefix' => '/v1'], function () {
+    Route::resource('employee', EmployeeController::class);
+    Route::resource('role', RoleController::class);
+    Route::resource('unit', UnitController::class);
+    Route::resource('auth', AuthController::class);
 });
