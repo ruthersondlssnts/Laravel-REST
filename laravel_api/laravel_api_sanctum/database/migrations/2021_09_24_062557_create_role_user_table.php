2a88e9feeed548550625f392e124a934bfa3d304
ruthersondlssnts <rutherson20@gmail.com>
2021-09-26 20:45:11 +0800

    change migration

--- laravel_api/laravel_api_sanctum/database/migrations/2021_09_24_062557_create_role_user_table.php
+++ laravel_api/laravel_api_sanctum/database/migrations/2021_09_24_062557_create_role_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserRoleTable extends Migration
+class CreateRoleUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUserRoleTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_role', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->integer('user_id');
